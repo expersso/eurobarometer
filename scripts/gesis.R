e287f21741d6af9e17eb03a09873b14ae6f0fd16
Expersso <expersso5@gmail.com>
2015-10-11 17:36:18 +0200

    

---
+++ scripts/gesis.R
@@ -0,0 +1,59 @@
+# Initiate Selenium server, specify Firefox profile to not ask when downloading
+gesis_setup <- function(download_dir) {
+
+  library(RSelenium)
+
+  # set firefox properties to not open download dialog
+  fprof <- makeFirefoxProfile(list(
+    browser.download.dir = paste0(getwd(), download_dir),
+    browser.download.folderList = 2L,
+    browser.download.manager.showWhenStarting = FALSE,
+    browser.helperApps.neverAsk.saveToDisk = "application/octet-stream"))
+
+  RSelenium::checkForServer()
+  RSelenium::startServer()
+  remDr <- remoteDriver(extraCapabilities = fprof)
+  remDr$open()
+
+}
+
+# Initial login
+gesis_login <- function(remDr,
+                        user = getOption("gesis_user"),
+                        pass = getOption("gesis_pass")) {
+
+  remDr$navigate("https://dbk.gesis.org/dbksearch/gdesc.asp")
+  remDr$findElement(using = "id", value = "loginContainer")$clickElement()
+
+  remDr$findElement(using = "name", "user")$sendKeysToElement(list(user))
+  remDr$findElement(using = "name", "pass")$sendKeysToElement(list(pass))
+  remDr$findElement(using = "id", "login")$clickElement()
+
+}
+
+# Go to download page
+gesis_download <- function(remDr, url) {
+  remDr$navigate(url)
+
+  # Click tab "Data & Download"
+  remDr$findElement("xpath", "//a[contains(text(), 'Documents')]")$clickElement()
+
+  # Click filename to download .dta file
+  remDr$findElement("xpath", "//a[contains(text(), 'dta')]")$clickElement()
+
+  # Input purpose and terms of use
+  remDr$switchToWindow(remDr$getWindowHandles()[[1]][2])
+
+  # Only check "accept terms of purpose" if unchecked
+  if(remDr$findElement("name",
+      "projectok")$getElementAttribute("checked")[[1]][1] != "true") {
+      remDr$findElement("name", "projectok")$clickElement()
+  }
+
+  remDr$findElement("xpath", "//option[@value='1']")$clickElement()
+  remDr$findElement("xpath", "//input[@value='Download']")$clickElement()
+
+  # Close Download window and switch back to first window
+  remDr$closeWindow()
+  remDr$switchToWindow(remDr$getWindowHandles()[[1]])
+}
